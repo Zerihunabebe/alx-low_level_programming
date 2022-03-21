7ad3228332b0051a6e0848e7e6a4fb32a0480e17
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-21 07:38:44 +0000

    a function that returns the length of strands

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,16 +6,16 @@
  *
  * Return: the length of the string
  */
-int _strlen(char *s)
+int _strlen(char *s);
 {
 	int i;
 
 	i = 0:
 
-		while (s[i] != '\0')
-		{
-			i++;
-		}
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
 	return (i);
 }
