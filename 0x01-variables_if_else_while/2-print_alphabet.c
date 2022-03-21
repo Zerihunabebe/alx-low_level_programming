6db81291d8c02345cd48a05c2168d8b82b0ec060
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-21 09:31:10 +0000

    My commit message for task two

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,18 @@
-#include "main.h"
-/**
- * mul -multiplies two integers
- * @a: print int
- * @b: print int
- * Return: Always 0.
- */
+#include <stdio.h>
 
-int mul(int a, int b)
+/**
+ * main - prints all the alphabets in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	return (a * b);
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
