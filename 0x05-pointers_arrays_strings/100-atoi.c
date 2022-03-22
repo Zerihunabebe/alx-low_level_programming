403c5258c812fd91cf501432f82af7329f32143f
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-22 18:29:43 +0000

    _atoi can convert a number in a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 	while (s[0] != '\0')
 	{
 		if (s[0] == '-')
-			sign_cacha *= -1;
+			sign_cache *= -1;
 		else if (s[0] >= '0' && s[0] <= '9')
 			result = (result * 10) + (s[0] - '0') * sign_cache;
 		else if (result)
