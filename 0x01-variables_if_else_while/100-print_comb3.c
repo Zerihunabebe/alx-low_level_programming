ca5fa526dbd1f37e747bc66227eb8d0ee6c3eef0
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-22 19:51:07 +0000

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,31 +11,22 @@ int main(void)
 	int number_right;
 
 
-	for (number_left = 56; number_right <= 78; number_left++)
+	for (number_left = 48; number_left <= 56; number_left++)
 	{
-		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+		for (number_right = 49; number_right <= 57; number_right++)
 		{
-
-			putchar(number_left);
-			putchar(number_right);
-
-
-			if ((number_left == 56) && (number_right == 78))
+			if (number_right > number_left)
 			{
-				break;
+				putchar(number_left);
+				putchar (number_right);
+				if (number_left != 56 || number_right != 56)
+			{
+				putchar(',');
+				putchar(' ');
 			}
-
-			putchar(',');
-			putchar(' ');
-
 		}
-
 	}
-
-	putchar('\n');
-
-
-	return (0);
-
-
+}
+putchar('\n');
+return (0);
 }
