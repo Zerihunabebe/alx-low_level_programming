4befc5b912dd6f629fbbccd7e2adb78cd87410e0
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-22 13:29:21 +0000

    return the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,8 @@
  * _strlen - This function returns the length of a string
  * @s: Variable that contains a character array to be counted
  * Return: Length of the string
- * */
+ */
+
 int _strlen(char *s)
 {
 	int i;
