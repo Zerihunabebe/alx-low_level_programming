9a1c05a9a35467626f60a6d7825c7ce173b3f481
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-22 13:06:49 +0000

    Commit for head.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -9,3 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
