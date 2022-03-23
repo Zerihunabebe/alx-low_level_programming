b74f261e779d24a5c52f07955d82d7017fc72956
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-23 05:31:22 +0000

    It checks whther the pass character is an alphabet (A - Z or a - z

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,6 +5,7 @@
  * _isalpha - checks whether the passed character is an alphabet (A-Z or a-z)
  * @c: The character to check
  *
+ * Return: 1 if c is an alphabet, otherwise Return: 0
  */
 int _isalpha(int c)
 {
