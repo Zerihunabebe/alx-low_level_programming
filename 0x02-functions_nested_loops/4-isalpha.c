d685c0ff437ae7b9c19b693a19930d432d67052c
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-23 05:19:37 +0000

    It chacks whether the passed characheter is an alphabet A - Z ora - z

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks whether the passed character is an alphabet (A-Z ora-z
+ * @c: The character to check
+ *
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
