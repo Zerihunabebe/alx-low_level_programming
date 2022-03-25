3391177b0e5aac5421d9ca361805f43a5bc8fc40
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-25 20:02:57 +0000

    My commit message or task 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,7 +2,8 @@
 
 #include "main.h"
 {
-	int n; int a[5];
+	int n;
+	int a[5];
 	int *p;
 
 	a[2] = 1024;
@@ -18,5 +19,5 @@
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-	return(0);
+	return (0);
 }
