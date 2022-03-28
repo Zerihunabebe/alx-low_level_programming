ea343489b6082cfdd1ab82a601bef87027180e1a
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-28 18:56:22 +0000

    THASK three

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,44 +2,34 @@
 #include <stdio.h>
 
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
+ * *_strspn - gets length of a prefix substring
+ * @s: string to be searched
+ * @accept: string containing value to be searched
+ *
+ * Return: returns number of bytes in intial segment
  */
-
 unsigned int _strspn(char *s, char *accept)
-
 {
-	int i, j;
-	int count = 0;
-	char *str1, *str2;
+	int a, b, c, value;
 
-	str1 = s;
-	str2 = accept;
+	c = 0;
 
-	i = 0;
-	while (str1[i] != '\0') /* Declaring WHILE*s */
+	for (a = 0 ; s[a] != '\0'; a++)
 	{
-		j = 0;
-		while (str2[j] != '\0') /* Declaring WHILE *accept */
+		value = 0; 
+		for (b = 0; accept[b] != '\0'; b++)
 		{
-			if (str2[j] == str1[i] /* Evaluate condition */
-					{
-					count++; /* count number */
-						   break;
-						   }
+			if (s[a] == accept[b])
+			{
+				c++;
+				value = 1;
+			}
+		}
+		if (value == 0)
+		{
+			return (c);
+		}
+	}
 
-						   j++; /* add j+1 */
-					}
-
-					if (s[i] != accept[j] /* If aren't equale */
-						{
-						break;
-						}
-
-						i++; /* add x+1 */
-						}
-
-						return (count); /* return the value of count */
-						}
+	return (0);
+}
