bc235b464516e877650a6326a26157fad1dbd8bc
Zerihunabebe <abebezerihunalx@gmail.com>
2022-03-30 08:26:18 +0000

    prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /*
- * _print_rev_recursion - Prints a sring in revers
- *
+ * _print_rev_recursion - Prints a sring in reverse
  * @s: Pointer to the string
  *
- * Return: void
- *
+ * Return: returns 0
  */
-
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
