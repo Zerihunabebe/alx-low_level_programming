5671dfc5b7d31e8839eb00645acec5a007ac3797
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-03 07:36:11 -0700

    Adds numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
- * main - adds numbers
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
+ * check_num - check - string there are gigit
+ * @str: array str
  *
- * Return: 0 if no error, else 1
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
-{
-	int a = 0, i, j;
 
-	for (i = 1, i < argc, i++)
+int check_num(char *str)
+
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
 	{
-		for (j = 0; argc[i][j]; j++)
+		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
-			if (!isdigit(argv[i][j]) == 0)
-					{
-						puts("Error");
-						return (1);
-					}
+			return (0);
 		}
+
+		count++;
 	}
-	for (i = 1; i < argc; i++)
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		a += atoi(argv[i]);
+		if(check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
+
+		/*Condition if one of the number contains sybols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
 	}
-	printf("%d\n", a);
+
+	printf("%d\n", sum); /*print sum*/
+
 	return (0);
 }
