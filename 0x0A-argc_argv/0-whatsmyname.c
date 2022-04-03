bec758aef00d2b143f5f2d8962c879951ad58114
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-03 05:29:57 -0700

    It prints program's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,41 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-void print_string(char *str);
-
 /**
- * main - prints its name
+ * main - prints program's name followed by new line
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
  *
- * @argc: Length of argv
- *
- * @argv Arrays of the program arguments
- *
- * Return: 0, success
+ * Return: Always (0)
  */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	print_string(argv[0]);
-	putchar('\n');
+	printf("%s\n", *argv);
 
 	return (0);
-}
-
-/**
- * print_string - Prints all char of a string
- *
- * @str: Pointer to string
- *
- * Return: void
- */
-
-void print_string(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		putchar(str[i]);
-		i++;
-	}
 }
