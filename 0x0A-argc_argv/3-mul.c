21423068eaa76a395378d8e6075b25f24b36dfae
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-03 08:20:39 -0700

    Can multiplyies numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,106 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+int _atoi(char *s);
+int multiply_two_nums(int num1, int num2);
+void print_number(int n);
 
 /**
- * main - prints the multiplication of two integers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if true, 1 if false
+ * main - Entry point
+ *
+ * @argc: Length of @argv
+ *
+ * @argv: Array of string of the arguments of this program
+ *
+ * Return: 0, Success
  */
+
 int main(int argc, char *argv[])
 {
-int a, b;
-if (argc == 3)
-{
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-printf("%d\n" a *b);
-return (0);
+	if (argc == 3)
+	{
+		print_number(multiply_two_nums(_atoi(argv[1]), _atoi(argv[2])));
+		_putchar('\n');
+		return (0);
+	}
+	else
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
+	}
 }
-printf("Error\n");
-return (1);
+
+/**
+ * _atoi - Convert a number in a string to an integer
+ *
+ * @s: String to be converted
+ *
+ * Return: Integer converted OR (0) if none
+ */
+
+int _atoi(char *s)
+{
+	int sign_cache = 1;
+	int result = 0;
+
+	while (s[0] != '\n')
+	{
+		if (s[0] == '-')
+			sign_cache *= -1;
+		else if (s[0] >= '0' && s[0] <= '9')
+			result = (result * 10) + (s[0] - '0');
+		else if (result)
+			break;
+
+		s++;
+	}
+
+	return (result * sign_cache);
+}
+
+/**
+ * multiply_two_nums - Multiplys two numbers
+ *
+ * @num1: A number
+ *
+ * @num2: A number
+ *
+ * Return: Result of num1 * num2
+ */
+
+int multiply_two_nums(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * print_number - Prints numbers chars
+ *
+ * @n: Integers params
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
