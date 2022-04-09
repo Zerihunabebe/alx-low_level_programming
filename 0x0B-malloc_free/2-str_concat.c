a7b4dd93c6ba81f4a7b3fa3ddf1e89c01aabcb77
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-09 12:51:57 -0700

    It can concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,45 @@
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * *_str_concat - conactenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
+ * str_concat - Concatenate two strings
  *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * @s1: A string to be concatenated
+ *
+ * @s2: A string to be concatenated
+ *
+ * Return: Pointer to newly allocated memory
+ * which is equal to @s1 + @s2 (SUCCESS) OR
+ * NULL (FAILURE)
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len 2 = 0;
+	unsigned int s1_length = 0;
+	unsigned int s2_length = 0;
+	char *alloc_mem;
+	unsigned int i, j;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
-		return (NULL);
+	if (s2 == NULL)
+		s2 = "";
+	while (*(s1 + s1_length) != '\0')
+		s1_length++;
 
-	i = 0;
-	j = 0;
+	while (*(s2 + s2_length) != '\0')
+		s2_length++;
 
-	if (s1)
-	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
+	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
+	if (alloc_mem == NULL)
+		return ('\0');
 
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] s2[j];
-			i++;
-			j++
-		}
-	}
-	s3[i] = '\0';
+	for (i = 0; i < s1_length; i++)
+		alloc_mem[i] = *(s1 + i);
 
-	return (s3);
+	for (j = 0; j < s2_length; j++, i++)
+		alloc_mem[i] = *(s2 + j);
+
+	return(alloc_mem);
+
 }
