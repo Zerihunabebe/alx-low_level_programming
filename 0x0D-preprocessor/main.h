e53d22883252a561af58d22865db0f2b789467ed
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-12 09:36:30 -0700

    My main

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include "0-object_like_macron.h"
+#include "0-object_like_macro.h"
 #include "1-pi.h"
-#include "3-function_like_macron.h"
+#include "3-function_like_macro.h"
 #include "4-sum.h"
 
 #endif
