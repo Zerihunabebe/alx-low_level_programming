bbdbc8e51f1a66aa14a55c36a35858d0dea8903c
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-13 10:52:46 -0700

    Prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,16 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * print_name - prints name
- * @name: name to be printed
- * @f: potnter to print function
+ * print_name - prints a name
+ * @name: string
+ * @f: function pointer
+ * Return: void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (f == NULL)
 		return;
 	f(name);
 }
