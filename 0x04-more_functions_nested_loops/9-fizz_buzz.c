913b83258a9eb94b52e89d2d3657485afd2d483f
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-18 22:05:58 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - This program prnts the numbers 1-1000, followed by a newline.
+ * But for multiples of 3, prints 'Fizz, insted of the number,
+ * for multiple of 5, prints 'Buzz', and for multiple of both,
+ * prints'FizzBuzz'
+ * Retiurn: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+
+		if (i != 100)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
