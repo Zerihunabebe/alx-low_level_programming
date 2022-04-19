fc93df6ec994c5038f0aaa5c7bc2d2cc8e846d5c
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-18 22:21:48 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,9 @@
 
 /**
  * main - This program prnts the numbers 1-100, followed by a newline.
- * But for multiples of 3, prints 'Fizz, insted of the number,
+ * But for multiples of 3, prints 'Fizz' instead of the number,
  * for multiple of 5, prints 'Buzz', and for multiple of both,
- * prints'FizzBuzz'
+ * prints 'FizzBuzz'
  * Retiurn: Always 0 (success)
  */
 int main(void)
