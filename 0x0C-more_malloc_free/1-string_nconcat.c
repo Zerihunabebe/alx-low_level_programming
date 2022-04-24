ed85292c4c0d18b6c96fd66c2271de3559025907
Zerihunabebe <abebezerihunalx@gmail.com>
2022-04-24 13:08:04 -0700

    conatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int j = 0;
 	unsigned int m;
-	unsigned p;
+	unsigned int p;
 	unsigned int k = 0;
 	unsigned int len;
 
