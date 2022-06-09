3c605d67d057424754d1634cf3684abdf998504e
Zerihunabebe <abebezerihunalx.com>
2022-06-08 23:15:16 -0700

    Inserts a new node at a given position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to head of list
+ * @idx index of the list where the new node should be added
+ * @n: data of the new node
+ *
+ * Return: the address of the new node, or Null if it faild
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to head of list
+ * @idx index of the list where the new node should be added
+ * @n: data of the new node
+ *
+ * Return: the address of the new node, or Null if it faild
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to head of list
+ * @idx index of the list where the new node should be added
+ * @n: data of the new node
+ *
+ * Return: the address of the new node, or Null if it faild
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}
