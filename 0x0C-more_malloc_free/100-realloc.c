41ed1a7a75d83d7fc9bd9567b84eb645216c18ee
Zerihunabebe <abebezerihunalx.com>
2022-06-17 08:51:06 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,13 +2,13 @@
 
 
 /**
- * _realloc - reallocates a memory block
- * @ptr: pointer to the memory previously allocated with a call to malloc
- * @old_size: size of ptr
- * @new_size: size of the new memory to be allocated
- *
- * Return: pointer to the address of the new memory block
- */
+* _realloc - reallocates a memory block
+* @ptr: pointer to the memory previously allocated with a call to malloc
+* @old_size: size of ptr
+* @new_size: size of the new memory to be allocated
+*
+* Return: pointer to the address of the new memory block
+*/
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
