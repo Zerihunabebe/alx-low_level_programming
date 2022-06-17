b7bb7034463c0c94179141d5d80c5f3946a7adcc
Zerihunabebe <abebezerihunalx.com>
2022-06-17 08:46:10 -0700

    Update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		temp_block = malloc(new_size);
 		return (temp_block);
 	}
-	else if (new_size == ol_size)
+	else if (new_size == old_size)
 		return (ptr);
 
 	else if (new_size == 0 && ptr != NULL)
